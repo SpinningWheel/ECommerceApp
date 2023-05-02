3c590818e7faae4ed66b0b03b360c4d9526eb1dc
ZEPHYRUS <ZEPHYRUS@ZEPHYRUS>
2023-05-02 18:16:17 +0400

    Add project files.

---
+++ ECommerceApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using ECommerceApp.Models;
+using ECommerceApp.Service;
+
+namespace ECommerceApp {
+    internal class Program {
+        static void Main(string[] args) {
+            var serv = new Services();
+            string line = "";
+            while (line != "stop") {
+                line = Console.ReadLine(); ;
+                var input = line.Split(" ");
+                var command = input[0];
+                switch (command) {
+
+                    case "save_product":
+                        if (checkForValidity(input.Count(), 4)) break;
+                        serv.SaveProduct(new Product() {
+                            Id = input[1],
+                            Name = input[2],
+                            Price = int.Parse(input[3])
+                        });
+                        break;
+
+                    case "purchase_product":
+                        if (checkForValidity(input.Count(), 4)) break;
+                        serv.PurchaseProduct(new Product() {
+                            Id = input[1],
+                            Quantity = int.Parse(input[2]),
+                            Price = int.Parse(input[3])
+                        });
+                        break;
+
+                    case "order_product":
+                        if (checkForValidity(input.Count(), 3)) break;
+                        serv.OrderProduct(new Product() {
+                            Id = input[1],
+                            Quantity = int.Parse(input[2])
+                        });
+                        break;
+                    
+                    case "get_quantity_of_product":
+                        if (checkForValidity(input.Count(), 2)) break;
+                        Console.WriteLine(serv.GetQuantityOfProduct(new Product() { Id = input[1] }));
+                        break;
+                    
+                    case "get_average_price":
+                        if (checkForValidity(input.Count(), 2)) break;
+                        Console.WriteLine(serv.GetAveragePrice(new Product() { Id = input[1] }));
+                        break;
+                    
+                    case "get_product_profit":
+                        if (checkForValidity(input.Count(), 2)) break;
+                        serv.GetProductProfit(new Product() { Id = input[1] });
+                        break;
+                    
+                    case "get_fewest_product":
+                        Console.WriteLine(serv.GetFewestProduct());
+                        break;
+                    
+                    case "get_most_popular_product":
+                        Console.WriteLine(serv.GetMostPopularProduct());
+                        break;
+                    
+                    case "exit":
+                        serv.Exit();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option !");
+                        break;
+                }
+            }
+        }
+        static bool checkForValidity(int size, int validSize) {
+            if (size != validSize) {
+                Console.WriteLine("Invalid Command");
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ECommerceApp/Service/Services.cs
@@ -0,0 +1,164 @@
+﻿using ECommerceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Service {
+    class Services {
+        private List<Product> _catalogDb = new List<Product>();
+        private List<Product> _purchasedDb = new List<Product>();
+        private List<Product> _orderedDb = new List<Product>();
+
+        public Services() {
+
+        }
+        public void SaveProduct(Product product) {
+            var resultProduct = _catalogDb.Where(c => c.Id.Equals(product.Id)).FirstOrDefault();
+            if (resultProduct == null) {
+                _catalogDb.Add(new Product {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Price = product.Price
+                });
+                return;
+            }
+            resultProduct.Name = product.Name;
+            resultProduct.Price = product.Price;
+        }
+        public void PurchaseProduct(Product product) {
+            var resultProduct = _catalogDb.Where(c => c.Id.Equals(product.Id)).FirstOrDefault();
+            if (resultProduct == null) {
+                Console.WriteLine("You have to register product beforehand in order to purchase it !");
+                return;
+            }
+            _purchasedDb.Add(product);
+            resultProduct.Quantity += product.Quantity;
+        }
+        public void OrderProduct(Product product) {
+            var orderedProduct = _catalogDb.Where(c => c.Id.Equals(product.Id)).FirstOrDefault();
+            if (orderedProduct == null) {
+                Console.WriteLine("No such product to sell ! ");
+                return;
+            }
+            product.Price = orderedProduct.Price;
+            product.Name = orderedProduct.Name;
+            _orderedDb.Add(product);
+            orderedProduct.Quantity -= product.Quantity;
+        }
+        public int GetQuantityOfProduct(Product product) {
+            var resultProd = _catalogDb.Where(c => c.Id.Equals(product.Id)).FirstOrDefault();
+            if (resultProd == null) {
+                Console.WriteLine("No such product ! ");
+                return 0;
+            }
+            return resultProd.Quantity;
+        }
+        public double GetAveragePrice(Product product) {
+            var purchasedQuantity = 0;
+            var purchasedBalance = 0;
+            var purchasedList = _purchasedDb.Where(c => c.Id.Equals(product.Id)).ToList();
+            if (purchasedList.Count == 0) {
+                return 0;
+            }
+            foreach (var p in purchasedList) {
+                purchasedQuantity += p.Quantity;
+                purchasedBalance += p.Price * p.Quantity;
+            }
+            return (double)purchasedBalance / (double)purchasedQuantity;
+        }
+        public void GetProductProfit(Product product) {
+            var avgPurchasedPrice = GetAveragePrice(product);
+            if (avgPurchasedPrice == 0) {
+                Console.WriteLine(avgPurchasedPrice);
+                return;
+            }
+            var orderedQuantity = 0;
+            var orderedBalance = 0;
+            var orderedList = _orderedDb.Where(c => c.Id.Equals(product.Id)).ToList();
+            if (orderedList.Count == 0) {
+                Console.WriteLine(0);
+                return;
+            }
+            foreach (var o in orderedList) {
+                orderedQuantity += o.Quantity;
+                orderedBalance += o.Price * o.Quantity;
+            }
+            var avgOrderedPrice = (double)orderedBalance / (double)orderedQuantity;
+            var avgProfitPU = avgOrderedPrice - avgPurchasedPrice;
+            var totalProfit = avgProfitPU * orderedQuantity;
+            Console.WriteLine(totalProfit);
+        }
+        public string GetFewestProduct() {
+            if (_catalogDb.Count == 0) return "There are no products on the catalog ! ";
+            var lowestQuantity = _catalogDb[0].Quantity;
+            var worstPName = _catalogDb[0].Name;
+            foreach (var p in _catalogDb) {
+                if (p.Quantity < lowestQuantity) {
+                    lowestQuantity = p.Quantity;
+                    worstPName = p.Name;
+                } 
+            }
+            return worstPName;
+        }
+        public string GetMostPopularProduct() {
+            int popQuantity = 0;
+            string popName = "";
+            if (_orderedDb.Count == 0) return "There are no products sold ! ";
+            foreach (var o in _orderedDb) {
+                int save = 0;
+                foreach (var p in _orderedDb) {
+                    if (p.Id.Equals(o.Id)) {
+                        save += p.Quantity;
+                    }
+                }
+                if (save > popQuantity) {
+                    popQuantity = save;
+                    popName = o.Name;
+                }
+            }
+            return popName;
+        }
+        public void Exit() {
+            Environment.Exit(0);
+        }
+
+        public void GetOrdersReport() {
+            var cogs = CalculateCOGS();
+            foreach (var o in _orderedDb) {
+                Console.WriteLine("Ordered  : " + o.Id + o.Name + o.Quantity + o.Price + ", COGS : " + cogs[o.Id]);
+            }
+        }
+        public void ExportOrdersReport(string path) {
+            string fileName = path + "\\OrdersReport.csv";
+            var cogs = CalculateCOGS();
+            try {
+                if (File.Exists(fileName)) File.Delete(fileName);
+                using (StreamWriter sw = File.CreateText(fileName)) {
+                    sw.WriteLine("Id," + "Name," + "Quantity," + "Price," + "COGS,");
+                    foreach (var o in _orderedDb) {
+                        sw.WriteLine(o.Id + "," + o.Name + "," + o.Quantity + "," + o.Price + "," + cogs[o.Id]);
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString);
+            }
+        }
+
+        public Dictionary<string, double> CalculateCOGS() {
+            var result = new Dictionary<string, double>();
+            foreach (var o in _orderedDb) {
+                int quantity = 0;
+                foreach (var c in _orderedDb) {
+                    if (o.Id.Equals(c.Id)) {
+                        quantity += c.Quantity;
+                    }                                        
+                }
+                double cogs = GetAveragePrice(o) * quantity;
+                if (!result.ContainsKey(o.Id)) result.Add(o.Id, cogs);
+            }
+            return result;
+        }
+    }
+}
